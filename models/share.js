3b27fad77c06ac5958fb8373e7a375c519ae53ae
AbirAzim <azim35-1713@diu.edu.bd>
2023-01-20 12:44:13 +0600

    initial commit

---
+++ models/share.js
@@ -0,0 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const shareSchema = new mongoose_1.Schema({
+    sharedBy: {
+        type: mongoose_1.SchemaTypes.ObjectId,
+        ref: 'Member',
+        required: [true, 'MemberId is required'],
+    },
+    shareTo: [
+        {
+            userId: {
+                type: mongoose_1.SchemaTypes.ObjectId,
+                ref: 'Member',
+            },
+            hasAccepted: Boolean,
+        },
+    ],
+    shareData: {
+        recipeId: { type: mongoose_1.SchemaTypes.ObjectId, ref: 'recipe' },
+        version: {
+            type: mongoose_1.SchemaTypes.ObjectId,
+            ref: 'recipeVersion',
+        },
+    },
+    isGlobal: {
+        type: Boolean,
+        default: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    notFoundEmails: [String],
+});
+const share = (0, mongoose_1.model)('share', shareSchema);
+exports.default = share;
---
+++ models/userCollection.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const userCollectionSchema = new mongoose_1.Schema({
+    name: {
+        type: String,
+        required: [true, 'name is required'],
+    },
+    slug: String,
+    image: String,
+    description: String,
+    userId: {
+        type: mongoose_1.SchemaTypes.ObjectId,
+        ref: 'Member',
+    },
+    recipes: [
+        {
+            type: mongoose_1.Schema.Types.ObjectId,
+            unique: true,
+            ref: 'Recipe',
+        },
+    ],
+    visible: { type: Boolean, default: true },
+    updatedAt: { type: Date, default: Date.now },
+});
+const UserCollection = (0, mongoose_1.model)('UserCollection', userCollectionSchema);
+exports.default = UserCollection;
